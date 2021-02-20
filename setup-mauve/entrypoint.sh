252dd344aa7bc5b9da5d41fb6f7c492f24ff1c53
llxlr <i@xhlr.top>
2021-02-21 02:10:17 +0800

    ok

--- setup-mauve/entrypoint.sh
+++ setup-mauve/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
-if [[ ! -z "$INPUT_NAME" ]] && [[ ! -z "$INPUT_ARGS" ]];then exit 1;fi
+if [[ ! -z ${INPUT_NAME} ]] && [[ ! -z ${INPUT_ARGS} ]];then exit 1;fi
 
-if [ $INPUT_NAME == "mauveAligner" ]; then
-  ./mauveAligner --version
-elif [ $INPUT_NAME == "progressiveMauve" ]; then
-  ./progressiveMauve --version
+if [ ${INPUT_NAME} == "mauveAligner" ]; then
+  ./mauveAligner ${INPUT_ARGS}
+elif [ ${INPUT_NAME} == "progressiveMauve" ]; then
+  ./progressiveMauve ${INPUT_ARGS}
 else
   echo 'The value of name is error.'
   exit 1
